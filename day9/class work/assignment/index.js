5fbf7dc2ed6e51d192495be610c0cb793e41f587
Nandanikumari400 <grdnandani2020@gmail.com>
2025-02-21 15:47:36 +0530

    to leartn ibm

---
+++ day9/class work/assignment/index.js
@@ -0,0 +1,30 @@
+
+
+
+vlet a 10, b="10"
+
+console.log(typeof(a))
+ 
+console.log(typeof(b))
+
+b-+b
+
+I
+
+if(a===b) {
+
+console.log("a is equal to b")
+
+}else{
+
+console.log("a is not equal to b")
+}
+myName()
+{
+    
+}
+console.log(a)
+function myname(){
+      var a=10
+      console.log(a)
+}
