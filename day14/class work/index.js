f5d7c46a52c85d2895f8e7a15aeb23519abc2562
Nandanikumari400 <grdnandani2020@gmail.com>
2025-03-01 21:44:01 +0530

    IBM

---
+++ day14/class work/index.js
@@ -0,0 +1,5 @@
+//create a custom function that will take array as input and return short array as input  and return shorted array
+//input arr=[3,6,9,2,56,34,98,56,23,12,90]
+//output arr=[2,3,6,9,12,23,34,56,56,90,98]
+let arr=[3,6,9,2,56,34,98,56,23,12,90]
+data.sort(a,b)=>a.name
